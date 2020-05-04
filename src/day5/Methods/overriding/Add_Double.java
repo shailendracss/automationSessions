83833fe68d6fa74f19202732e4a2f132d3a72c46
shailu <shailu@ubu>
2020-05-04 18:17:42 +0530

    To Fire SonarQube Cloud Analysis

--- src/day5/Methods/overriding/Add_Double.java
+++ src/day5/Methods/overriding/Add_Double.java
@@ -5,6 +5,7 @@ public class Add_Double extends Add_Int{
 	//Overriding the parent class method, and enhancing its functionality
 	public void add(int a, int b){
 		System.out.println("OUTPUT= "+(a+b));
+		
 	}
 	
 	public void add(double a, double b){
